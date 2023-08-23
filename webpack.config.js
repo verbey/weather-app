3f70f503e26f9d249bb9cf367c92de044950b505
Verbiturum <user@example.com>
2023-08-24 02:52:13 +0300

    Add babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,18 @@ module.exports = {
 
 				type: 'asset/resource',
 			},
+			{
+				test: /\.(?:js|mjs|cjs)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [
+							['@babel/preset-env', { targets: "defaults" }]
+						]
+					}
+				}
+			},
 		],
 	},
 };
