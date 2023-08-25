ea9b5ef1bb74827a80486620a72c530edc38b8ce
Verbiturum <user@example.com>
2023-08-26 00:42:15 +0300

    Install babel-env preset

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-	entry: "./src/index.js",
+	entry: "./src/script.js",
 	output: {
 		filename: "script.js",
 		path: path.resolve(__dirname, "dist"),
