55bb3f9b8ed3c16b1212accc3637ef7fdb1a81ed
Verbiturum <user@example.com>
2023-08-26 19:04:35 +0300

    Add Dom module

---
+++ src/modules/dom.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const Dom = (() => {
+	const cityName = document.querySelector(".cityName");
+	const country = document.querySelector(".country");
+	const localtime = document.querySelector(".localtime");
+
+	const temperature = document.querySelector(".temperature");
+	const weather = document.querySelector(".weather");
+
+	const feelsLike = document.querySelector(".feelsLike");
+	const humidity = document.querySelector(".humidity");
+	const wind = document.querySelector(".wind");
+
+	return {};
+})();
+
+export default Dom;
