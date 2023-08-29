3b06e083c49c151b130f88725cd7271880fe4aac
Verbiturum <user@example.com>
2023-08-29 15:58:17 +0300

    Move setBackground function into module

--- src/modules/background.js
+++ src/modules/background.js
@@ -7,13 +7,17 @@ import foggy from "../images/foggy-weather.jpg";
 import snowy from "../images/snowy-weather.jpg";
 import defaultWeather from "../images/default-weather.jpg";
 
-const setBackground = (weather) => {
-	if (/sunny|clear/gi.test(weather)) document.body.style.backgroundImage = `url(${sunny})`;
-	else if (/cloud|overcast/gi.test(weather)) document.body.style.backgroundImage = `url(${cloudy})`;
-	else if (/rain|shower|drizzle/gi.test(weather)) document.body.style.backgroundImage = `url(${rainy})`;
-	else if (/fog|mist/gi.test(weather)) document.body.style.backgroundImage = `url(${foggy})`;
-	else if (/snow|blizzard|sleet/gi.test(weather)) document.body.style.backgroundImage = `url(${snowy})`;
-	else document.body.style.backgroundImage = `url(${defaultWeather})`;
-};
+const Background = (() => {
+	const setBackground = (weather) => {
+		if (/sunny|clear/gi.test(weather)) document.body.style.backgroundImage = `url(${sunny})`;
+		else if (/cloud|overcast/gi.test(weather)) document.body.style.backgroundImage = `url(${cloudy})`;
+		else if (/rain|shower|drizzle/gi.test(weather)) document.body.style.backgroundImage = `url(${rainy})`;
+		else if (/fog|mist/gi.test(weather)) document.body.style.backgroundImage = `url(${foggy})`;
+		else if (/snow|blizzard|sleet/gi.test(weather)) document.body.style.backgroundImage = `url(${snowy})`;
+		else document.body.style.backgroundImage = `url(${defaultWeather})`;
+	};
 
-export default setBackground;
+	return { setBackground };
+})();
+
+export default Background;
